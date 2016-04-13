f5383fb625bf343b69ff3e745622c9de8d0b5e95
littledan45 <littledan45@gmail.com>
2016-04-13 16:29:34 -0400

    dev-app tests

--- dev-app/devApp.lua
+++ dev-app/devApp.lua
@@ -1,4 +1,12 @@
-local fs = require("filesystem")
+local component = require("component")
+local fs = component.filesystem
+
+function checkCfg()
+  if fs.exists("/usr/dev-app/config.lua") then
+    print("Yes")
+  else
+    print("No")
+end
 
 for k,d = in pairs(fs.list("/")) do
   print(k, d)
