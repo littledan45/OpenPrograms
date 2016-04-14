161ffe0ade8b59d048da96e48aab556256b4ef1d
littledan45 <littledan45@gmail.com>
2016-04-14 11:04:23 -0400

    added config making function

--- oc-agri-seed-enhancer/oc-agri-seed-enhancer.lua
+++ oc-agri-seed-enhancer/oc-agri-seed-enhancer.lua
@@ -1,28 +1,52 @@
 local component = require("component")
+local term = require("term")
+local io = require("io")
+local serial = require("serialization")
+--local rs = component.redstone
 -- add Geolyzer address
 -- TODO fix Geolyzer proxies
-local geoLeft = component.proxy("")
-local geoCenter = component.proxy("")
-local geoRight = component.proxy("")
--- add Redstone address
-local rs = component.proxy("")
+-- example geolyzer[centerAddress, centerFace]
+local geolyzer = {}
 
--- What side are the Crop stick on
-local constGeolyzerFace = 4
-
+-- seed tables for each geolyzer
 local tableGeoLeft = {}
 local tableGeoCenter = {}
 local tableGeoRight = {}
 
-function geoAnalyze(geo, direction)
-  return geo.analyze(direction)
+function initMakeGeoFile()
+  local geoTab = {}
+  for _add,_ in pairs(component.list("geolyzer")) do
+    table.insert(geoTab, _add)
+  end
+  for k,d in pairs(geoTab) do
+    print(k, d)
+  end
+  -- Get geolyzer address from user
+  term.write("Insert the number for the Center Geolyzer\n")
+  local geoCenterAddress = tonumber(term.read())
+  term.write("Insert the number for the Left Geolyzer\n")
+  local geoLeftAddress = tonumber(term.read())
+  term.write("Insert the number for the Right Geolyzer\n")
+  local geoRightAddress = tonumber(term.read())
+  term.write("What side are the Crops? (use Minecraft directions 0-3)\n")
+  local geoFace = tonumber(term.read())
+  -- Add geolyzer address & face to main table
+  geolyzer.centerAddress = component.proxy(geoTab[geoCenterAddress])
+  geolyzer.leftAddress = component.proxy(geoTab[geoLeftAddress])
+  geolyzer.rightAddress = component.proxy(geoTab[geoRightAddress])
+  geolyzer.face = geoFace
+  -- Write geolyzer table to file
+  local serialGeolyzer = serial.serialize(geolyzer)
+  file = io.open("/home/config/oc-agri-seed-enhancer.cfg", "w")
+  file:write(serialGeolyzer)
+  file:close()
 end
 
 function init()
-  tableGeoLeft = geoAnalyze(geoLeft, constGeolyzerFace)
-  tableGeoRight = geoAnalyze(geoRight, constGeolyzerFace)
-
+  initMakeGeoFile()
   -- if air
 
   -- if crops
 end
+
+init()
