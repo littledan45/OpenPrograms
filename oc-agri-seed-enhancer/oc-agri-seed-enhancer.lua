7e313d8aeb23a2d95e216d193eaf553db2eb334e
littledan45 <littledan45@gmail.com>
2016-04-14 17:31:54 -0400

    redid everything

--- oc-agri-seed-enhancer/oc-agri-seed-enhancer.lua
+++ oc-agri-seed-enhancer/oc-agri-seed-enhancer.lua
@@ -3,68 +3,9 @@ local term = require("term")
 local io = require("io")
 local serial = require("serialization")
 local fs = component.filesystem
---local rs = component.redstone
--- example analyzer[centerAddress, centerFace]
-local analyzer = {}
+local API = require("ocAgriSeedEnhancerAPI")
 
-function initMakeAnaFile()
-  local anaTab = {}
-  for _add,_ in pairs(component.list("agricraft_peripheral")) do
-    table.insert(anaTab, _add)
-  end
-  for k,d in pairs(anaTab) do
-    print(k, d)
-  end
-  -- Get analyzer address from user
-  local directions = {"NORTH", "SOUTH", "WEST", "EAST"}
-  term.write("Insert the number for the Center Analyzer\n")
-  local anaCenterAddress = tonumber(term.read())
-  term.write("Insert the number for the Left Analyzer\n")
-  local anaLeftAddress = tonumber(term.read())
-  term.write("Insert the number for the Right Analyzer\n")
-  local anaRightAddress = tonumber(term.read())
-  for k,d in pairs(directions) do
-    term.write(k, d)
-  end
-  term.write("What side are the Crops?\n")
-  local anaFace = tonumber(term.read())
-  -- Add analyzer address & face to main table
-  analyzer.centerAddress = component.proxy(anaTab[anaCenterAddress])
-  analyzer.leftAddress = component.proxy(anaTab[anaLeftAddress])
-  analyzer.rightAddress = component.proxy(anaTab[anaRightAddress])
-  analyzer.face = directions[anaFace]
-  -- Write analyzer table to file
-  if fs.exists("/home/config") then
-    print("config folder exists")
-  else
-    fs.makeDirectory("/home/config")
-    print("made config directory")
-  end
-  local serialanalyzer = serial.serialize(analyzer)
-  local file = io.open("/home/config/oc-agri-seed-enhancer.cfg", "w")
-  file:write(serialanalyzer)
-  file:close()
-end
-
-function initGetAnaFile()
-  if fs.exists("/home/config/oc-agri-seed-enhancer.cfg") then
-    local file = io.open("/home/config/oc-agri-seed-enhancer.cfg", "r")
-    local serialanalyzer = file:read()
-    analyzer = serial.unserialize(serialanalyzer)
-  else
-    print("Config file dosen't exist but why did I Run?")
-  end
-end
-
-function init()
-  term.clear()
-  if fs.exists("/home/config/oc-agri-seed-enhancer.cfg") then
-    initGetAnaFile()
-  else
-    initMakeAnaFile()
-  end
---check if block is air or seed
-
-end
-
-init()
+API.checkConfig("analyzerLeft")
+--local analyzerLeft component.proxy(--TODO)
+--local analyzerCenter component.proxy(--TODO)
+--local analyzerRight component.proxy(--TODO)
---
+++ oc-agri-seed-enhancer/ocAgriSeedEnhancerAPI.lua
@@ -0,0 +1,50 @@
+local component = require("component")
+local io = require("io")
+local fs = require("filesystem")
+local serial = require("serialization")
+
+local API = {}
+
+function getAnalyzer(configFileName)
+  local _analyzers = {}
+  local _repeater = 1
+  for add,_ pairs(component.list("agricraft_peripheral")) do
+    _analyzers[_repeater] = add
+    _repeater = _repeater + 1
+  end
+
+  for k,d in pairs(_analyzers) do
+    print(k, d)
+  end
+
+  print("Please select the", configFileName)
+  local _id = tonumber(io.read())
+  return _analyzers[_id]
+end
+
+function writeConfig(fileName)
+  local tempTable = {}
+  tempTable[fileName] = getAnalyzer(fileName)
+  local sTempTable = serial.serialize(tempTable)
+  local file = io.open("/home/config/"..fileName.."cfg", "w")
+  file:write(sTempTable)
+  file:close()
+end
+
+function readConfig()
+--TODO
+end
+
+function API.checkConfig(fileName)
+  if fs.exists("/home/config") then
+    print("config exists")
+  else
+    fs.makeDirectory("/home/config")
+  end
+  if fs.exists("/home/config/"..fileName..".cfg") then
+    -- it exists TODO have to add readConfig
+  else
+    writeConfig(fileName)
+end
+
+return API
