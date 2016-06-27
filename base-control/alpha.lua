5c0f6b24075f46497b91f65b28aefdb8a67b32fc
LittleDan45 <littledan45@gmail.com>
2016-06-27 19:07:11 -0400

    added alpha program

---
+++ base-control/alpha.lua
@@ -0,0 +1,61 @@
+local event = require("event")
+local component = require("component")
+local rs = component.redstone
+local colors = require("colors")
+local sides = require("sides")
+
+local rsPower = colors.red
+local sidePower = sides.east
+
+_,_,p,m = event.pull("chat_message")
+
+print(p,m)
+
+function main()
+	if auth(p) == true then
+		result = get_command(m)
+		if result["key"] == true then
+			if result["message"] == "shutdown_power" then
+				shutdown_power()
+				print("Shuting down Power")
+			elseif result["message"] == "startup_power" then
+				startup_power()
+				print("Starting Up Power")
+			end
+		end
+	end
+end
+
+function auth(user)
+  if user == "littledan45" then
+  	return true
+	else
+		return false
+  end
+end
+
+function get_command(message)
+	message_tab = {}
+	for s in string.gmatch(message, "%S+") do
+		if s == "alpha" then
+			message_tab["key"] = true
+		else
+			if message_tab["key"] == true then
+				message_tab["message"] = s
+			else
+				break
+			end
+		end
+	end
+	return message_tab
+end
+
+function shutdown_power()
+	rs.setBundledOutput(sidePower, rsPower, 0)
+end
+
+function startup_power(rsColor)
+	rs.setBundledOutput(sidePower, rsPower, 15)
+end
+
+main()
