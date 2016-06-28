768f05327d92b90784b34e96535496e083f4dccf
LittleDan45 <littledan45@gmail.com>
2016-06-28 12:24:23 -0400

    Added touch support

--- base-control/alpha.lua
+++ base-control/alpha.lua
@@ -1,30 +1,69 @@
 local event = require("event")
+local term = require("term")
 local component = require("component")
+local gpu = component.gpu
 local rs = component.redstone
 local colors = require("colors")
 local sides = require("sides")
 
+local w, h = gpu.getResolution()
+local e, p, m
 local rsPower = colors.red
 local sidePower = sides.east
-
-_,_,p,m = event.pull("chat_message")
-
-print(p,m)
+local current = false
 
 function main()
-	rsControl()
+	term.clear()
+	update_mon()
+	while true do
+		e,_,p,m = event.pull()
+		handle_event()
+		update_mon()
+	end
 end
 
-function rsControl()
-	if auth(p) == true then
-		result = get_command(m)
-		if result["key"] == true then
-			if result["message"] == "shutdown_power" then
-				shutdown_power()
-				print("Shuting down Power")
-			elseif result["message"] == "startup_power" then
-				startup_power()
-				print("Starting Up Power")
+function update_mon()
+	if rs.getBundledOutput(sidePower, rsPower) == 0 then
+		if current == true then
+			generate_mon(colors.red)
+			current = false
+		end
+	else
+		if current == false then
+			generate_mon(colors.green)
+			current = true
+		end
+	end
+end
+
+function generate_mon(color)
+	gpu.setBackground(color, true)
+	gpu.fill(w/2-7, 2, 15, 3, " ")
+	gpu.set(w/2-2, 3, "Power")
+	gpu.setBackground(colors.black, true)
+end
+
+function handle_event()
+	--e,_,p,m = event.pull()
+	if e == "chat_message" then
+		if auth(p) == true then
+			result = get_command(m)
+			if result["key"] == true then
+				if result["message"] == "shutdown_power" then
+					shutdown_power()
+				elseif result["message"] == "startup_power" then
+					startup_power()
+				end
+			end
+		end
+	elseif e == "touch" then
+		if p > w/2-8 and p < w/2+16 then
+			if m > 1 and m < 5 then
+				if rs.getBundledOutput(sidePower, rsPower) == 0 then
+					startup_power()
+				else
+					shutdown_power()
+				end
 			end
 		end
 	end
@@ -58,7 +97,7 @@ function shutdown_power()
 	rs.setBundledOutput(sidePower, rsPower, 0)
 end
 
-function startup_power(rsColor)
+function startup_power()
 	rs.setBundledOutput(sidePower, rsPower, 15)
 end
 
